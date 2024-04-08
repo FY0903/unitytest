4e81a93d4b07d126bdc0c92a6c04e6017ff82072
FY0903 <158162451+FY0903@users.noreply.github.com>
2024-04-08 19:22:50 +0900

    04/08/19:22 いらないから削除

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -14,6 +14,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (Mathf.Cos(Time.time * 2), Mathf.Sin(Time.time * 2), 0);
+        transform.position = new Vector3 (Mathf.Cos(Time.time * 10), Mathf.Sin(Time.time * 10), 0);
     }
 }
